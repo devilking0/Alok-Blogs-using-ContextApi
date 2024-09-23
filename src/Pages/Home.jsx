792c6c19e8e6e5fd747a605622809fa62d4f0f81
Alok Prajapati <78910289+devilking0@users.noreply.github.com>
2024-09-23 21:46:22 +0530

    Uploaded complete blog website

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Blogs from '../components/Blogs'
+import Header from '../components/Header'
+import Pagination from '../components/Pagination'
+
+const Home = () => {
+  return (
+    <div className='flex flex-col'>
+      <Header/>
+      <div className=''>
+        <Blogs/>
+        <Pagination/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="py-4 border-b-2 border-b-gray-300 drop-shadow-md top-0 inset-x-0 bg-white">
+      <h1 className="font-bold text-3xl uppercase text-center">
+        Alok Blogs
+      </h1>
+    </header>
+  );
+}
